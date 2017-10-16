554e85e74bd30003c9f58adf7dee0f150f9be6a6
abel <abelcarreras83@gmail.com>
2017-10-16 12:00:38 +0900

    minor details

--- montemodes/classes/results.py
+++ montemodes/classes/results.py
@@ -141,8 +141,8 @@ class MonteCarlo:
 class Conditions:
     def __init__(self,
                  number_of_cycles=100000,
-                 kb=0.0019872041, # kcal/mol
-                 temperature_frequency_relation=0.694989, #K/cm
+                 kb=0.0019872041,  # kcal/mol
+                 temperature_frequency_relation=0.694989,  # K/cm
                  temperature=None,
                  initial_expansion_factor=None,
                  acceptation_regulator=1.0,
