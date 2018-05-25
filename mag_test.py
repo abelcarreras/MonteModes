d3b431b2ab59ceed6ca9c8ae2f72f0e21608e8b0
abel <abelcarreras83@gmail.com>
2018-05-25 02:32:18 +0200

    minor modifications in mag_test example

--- mag_test.py
+++ mag_test.py
@@ -7,21 +7,22 @@ import montemodes.functions.methods as meth
 import montemodes.classes.results as res
 import montemodes.analysis.magnetism as magnetism
 
-bs_singlet = meth.Gaussian(methodology='uhf',
+bs_singlet = meth.Gaussian(methodology='hf',
                            internal=False,
                            multiplicity=1,
                            guess='mix',
-                           alter={'alpha':[[1, 2],  # if unrestricted calculation, alter becomes a dictionary
-                                           [3, 4]],
-                                  'beta' :[[1, 2]]})
+                           alter=[[1, 2]])  # if alter is defined, 'guess=alter' keyword is automatically set
 
-triplet = meth.Gaussian(methodology='pm6',
+
+triplet = meth.Gaussian(methodology='uhf',
                         internal=False,
                         multiplicity=3,
-                        alter=[[1, 2]])  # if alter is defined, 'guess=alter' keyword is automatically set
+                        alter={'alpha': [[1, 2],  # if unrestricted calculation, alter becomes a dictionary
+                                         [3, 4]],
+                               'beta': [[1, 2]]})
 
 conditions = res.Conditions(temperature=300,
-                            number_of_cycles=2,
+                            number_of_cycles=100,
                             initial_expansion_factor=0.01,  # modes : 10 , cart: 0.01
                             acceptation_regulator=0.08,
                             number_of_values_for_average=93,
@@ -33,7 +34,7 @@ molecule.charge = -1
 simulation = res.MonteCarlo(molecule)
 result = monte.calculate_MonteCarlo(simulation, conditions, alteration_type='cartesian')
 
-j_list = magnetism.get_magnetic_coupling_trajectory(result.trajectory[1:], bs_singlet, triplet)
+j_list = magnetism.get_magnetic_coupling_trajectory(result.trajectory[10:], bs_singlet, triplet)
 io_monte.write_list_to_file(j_list, 'j_coupling.txt', label='J')
 
 print ('Average J: {}'.format(np.average(j_list)))
